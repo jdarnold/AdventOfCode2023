c348495171f38dcb0f6f0db92ed254431dc758ce
sysgbs-jon <143430129+sysgbs-jon@users.noreply.github.com>
2023-12-03 12:04:43 -0500

    Skeleton
    Add a template for future AoC projects, because they usually require opening and parsing a file

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Advent Of Code2023
+# Advent Of Code 2023
 
-I'm doing this in C# this year.
+For [Advent of Code 2023](https://adventofcode.com/2023)
+
+I'm doing this in [C#](https://learn.microsoft.com/en-us/dotnet/csharp/) this year.
+
+Please join my [private leaderboard](https://adventofcode.com/2023/leaderboard/private/view/335841)! The code is 335841-f28d1b57 - it is absolutely not competitive! Just fun to see how everyone is doing.
---
+++ Skeleton/Program.cs
@@ -0,0 +1,26 @@
+﻿// Pretty much every AoC project starts with this code, so let's create a template for it.
+class Program
+{
+    static void Main()
+    {
+        string filePath = "../../../input.txt";
+
+        // Check if the file exists
+        if (File.Exists(filePath))
+        {
+            // Read all lines from the file
+            string[] lines = File.ReadAllLines(filePath);
+
+            // Display the content
+            Console.WriteLine("File Content:");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        else
+        {
+            Console.WriteLine($"File not found: {filePath}");
+        }
+    }
+}
